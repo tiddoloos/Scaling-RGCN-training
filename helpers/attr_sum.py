14994bec5d5f4393bda78d770f5ac2402abddca1
tiddoloos <tiddoloos@hotmail.com>
2022-03-23 12:12:29 +0100

    working attr_sum.py

--- helpers/attr_sum.py
+++ helpers/attr_sum.py
@@ -1,8 +1,5 @@
-import rdfextras
-import os
-from rdflib import ConjunctiveGraph, Graph
-from rdflib import URIRef, BNode
-# rdfextras.registerplugins() # so we can Graph.query()
+from rdflib import Graph
+
 
 def make_graph_from_nt(input_data):
     g = Graph()
@@ -11,11 +8,10 @@ def make_graph_from_nt(input_data):
     return g
 
 def make_attr_sum(graph):
-
     # was there in allessndro's SPARQL paper
     # PREFIX rdf: <http ://www.w3. org/1999/02/22=rdf=syntax=ns#>
     # found PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> in paper pf Campinas
-    
+
     attr_query = """
     PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
     CONSTRUCT {
@@ -24,23 +20,23 @@ def make_attr_sum(graph):
     WHERE
     {
         {
-            SELECT ?s (SHA1(group concat(?p; separator = ”,”)) as ?sID) WHERE {
+            SELECT ?s (SHA1(group_concat(?p; separator = ",")) as ?sID) WHERE {
                 SELECT DISTINCT ?s ?p {
-                    ?s ?p ? _
+                    ?s ?p ?_
                 } order by ?p
             } group by ?s
         }
-        BIND(URI(CONCAT(”http://example.org/”, ?sID)) AS ?hs)
+        BIND(URI(CONCAT("http://example.org/", ?sID)) AS ?hs)
         ?s ?p ?o
         OPTIONAL { 
             {
-                SELECT ?o (SHA1(group concat(?p; separator = ”,”)) as ?oID) WHERE { 
+                SELECT ?o (SHA1(group_concat(?p; separator = ",")) as ?oID) WHERE { 
                     SELECT DISTINCT ?o ?p {
-                        ?o ?p ? _
+                        ?o ?p ?_
                     } order by ?p
                 } group by ?o
             }
-            BIND(URI(CONCAT(”http://example.org/”, ?oID)) AS ?ho2)
+            BIND(URI(CONCAT("http://example.org/", ?oID)) AS ?ho2)
         }
         BIND(IF(BOUND(?oID), ?ho2, ?o) AS ?ho)
     }
@@ -49,42 +45,10 @@ def make_attr_sum(graph):
     print(results)
     # return [str(result[0]) for result in results]
 
+def main_attr_sum(filename):
+    graph = make_graph_from_nt(filename)
+    print(len(graph))
+    sum_graph = make_attr_sum(graph)
+
 filename = "../../data/AIFB/raw/aifb_stripped.nt"
-graph = make_graph_from_nt(filename)
-print(len(graph))
-sum_graph = make_attr_sum(graph)
-
-# g=rdflib.Graph()
-# g.parse(filename)
-# g = make_graph_from_nquads(filename)
-# results = g.query("""
-# PREFIX rdf: <http ://www.w3. org/1999/02/22=rdf=syntax=ns#>
-# CONSTRUCT {
-#     ?hs ?p ?ho .
-# }
-# WHERE
-# {
-#     {
-#         SELECT ?s (SHA1(group concat(?p; separator = ”,”)) as ?sID) WHERE {
-#             SELECT DISTINCT ?s ?p {
-#                 ?s ?p ? _
-#                 } order by ?p
-#             } group by ?s
-#         }
-#         BIND(URI(CONCAT(”http://example.org/”, ?sID)) AS ?hs)
-#         ?s ?p ?o
-#         OPTIONAL { 
-#             {
-#                 SELECT ?o (SHA1(group concat(?p; separator = ”,”)) as ?oID) WHERE { 
-#                     SELECT DISTINCT ?o ?p {
-#                         ?o ?p ? _
-#                     } order by ?p
-#                 } group by ?o
-#             }
-#             BIND(URI(CONCAT(”http://example.org/”, ?oID)) AS ?ho2)
-#         }
-#         BIND(IF(BOUND(?oID), ?ho2, ?o) AS ?ho)
-# }
-# """)
-
-# # print(results)
+main_attr_sum(filename)
