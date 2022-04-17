c07d5ca9af676ece1da82516727790f8f633661d
tiddoloos <tiddoloos@hotmail.com>
2022-04-17 17:16:31 +0200

    refactor

--- main.py
+++ main.py
@@ -7,19 +7,19 @@ def initialize_training() -> None:
     the weights of the summary model will be transferd to a new model for training on the original graph.
     Also a benchmark experiment is conducted.
     """
-    
+
+    hidden_l = 16
     epochs = 50
     weight_d = 0.0005
     lr = 0.01
-    hidden_l=16
 
     # Transfer learning expriment
     trainer = modelTrainer('AIFB', hidden_l)
     results_transfer = trainer.main_modelTrainer(epochs, weight_d, lr, benchmark=False)
-    # results_dict['Summary graph loss'], results_dict['Orginal graph accuracy'], results_dict['Orginal graph loss'] = trainer_trans.main_modelTrainer(epochs, weight_d, lr, benchmark=False)
-
+   
     # Benchmark
     results_benchmark = trainer.main_modelTrainer(epochs, weight_d, lr, benchmark=True)
+
     results = {**results_transfer, **results_benchmark}
 
     plot_main('AIFB', results, epochs)
