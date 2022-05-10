2876fc1738e356f5b99b3997e91bfe5cda8f5772
tiddoloos <tiddoloos@hotmail.com>
2022-05-10 10:23:03 +0200

    jobupdate

--- job_script.sh
+++ job_script.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 #SBATCH --job-name=test_run
-#SBATCH -N 1 --ntasks-per-node=16
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=2
+#SBATCH --gpus=1
+#SBATCH --partition=gpu_shared
 #SBATCH -t 100:00:00
+#SBATCH --mail-type=ALL
 #SBATCH --mail-user=t.j.loos@student.vu.nl
+
+module purge all
+module load 2021
+module load Anaconda3/2021.05
 
 source /home/${USER}/.bashrc
 source activate scaling_rgcn
