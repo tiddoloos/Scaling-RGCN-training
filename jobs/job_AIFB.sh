d3954fe49aa1d2b10ed60322447b608131d0f512
tiddoloos <tiddoloos@hotmail.com>
2022-05-11 14:27:10 +0200

    job copy fix

--- jobs/job_AIFB.sh
+++ jobs/job_AIFB.sh
@@ -17,5 +17,4 @@ mkdir ./output_dir/AIFB
 python /home/loost/RGCN_MscThesis_TiddoLoos/main.py -dataset AIFB
 
 #Copy output directory from scratch to results folder on local machine
-scp -r loost@lisa.surfsara.nl:./output_dir/AIFB /Users/tiddo/Documents/Msc\ Artificial\ Intelligence/Thesis_RGCN/RGCN_MscThesis_TiddoLoos/results
-# scp output_dir/* /home/loost/output_dir/AIFB
+scp ./output_dir/* /home/loost/output_dir/AIFB
