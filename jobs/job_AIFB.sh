d00104ef81ddaaa64fdfc8b50f444b9d8e72e65d
tiddoloos <tiddoloos@hotmail.com>
2022-05-11 12:44:33 +0200

    AIFB job path fix

--- jobs/job_AIFB.sh
+++ jobs/job_AIFB.sh
@@ -19,5 +19,5 @@ cd "$TMPDIR"/
 python /home/loost/RGCN_MscThesis_TiddoLoos/main.py -dataset AIFB
 
 #Copy output directory from scratch to results folder on local machine
-scp loost@lisa.surfsara.nl:output_dir/* /Users/tiddo/Documents/Msc Artificial Intelligence/Thesis_RGCN/RGCN_MscThesis_TiddoLoos/results/AIFB
+scp loost@lisa.surfsara.nl:output_dir/* /Users/tiddo/Documents/Msc\ Artificial\ Intelligence/Thesis_RGCN/RGCN_MscThesis_TiddoLoos/results/AIFB
 
