09c5342ed6decb78d6dc37ebae6aebe264b1936d
tiddoloos <tiddoloos@hotmail.com>
2022-05-11 13:36:24 +0200

    job copy statement

--- jobs/job_AIFB.sh
+++ jobs/job_AIFB.sh
@@ -18,5 +18,5 @@ cd "$TMPDIR"/
 python /home/loost/RGCN_MscThesis_TiddoLoos/main.py -dataset AIFB
 
 #Copy output directory from scratch to results folder on local machine
-scp loost@lisa.surfsara.nl:output_dir/* /Users/tiddo/Documents/Msc\ Artificial\ Intelligence/Thesis_RGCN/RGCN_MscThesis_TiddoLoos/results/AIFB
-scp output_dir/* /home/loost/output_dir/AIFB
+scp loost@lisa.surfsara.nl:"$TMPDIR"/output_dir/* /Users/tiddo/Documents/Msc\ Artificial\ Intelligence/Thesis_RGCN/RGCN_MscThesis_TiddoLoos/results/AIFB
+# scp output_dir/* /home/loost/output_dir/AIFB
