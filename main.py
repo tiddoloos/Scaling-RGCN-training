36eb0bd76e11aba710af5fa4abcba08973f0ee71
tiddoloos <tiddoloos@hotmail.com>
2022-05-18 14:38:10 +0200

    refactor+emdding model == baselinemodel

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 import argparse
 
 from copy import deepcopy
-from collections import defaultdict
-from typing import Callable, Dict, List
+from typing import Callable, Dict
 
 from graphdata.graphData import Dataset
 from helpers.processResults import plot_and_save, print_max_result
 from helpers import timing
 from model.embeddingTricks import stack_embeddings, sum_embeddings, concat_embeddings
-from model.models import Emb_Layers, Emb_MLP_Layers, Emb_ATT_Layers, BaseLayers
+from model.models import Emb_Layers, Emb_MLP_Layers, Emb_ATT_Layers
 from model.modelTrainer import Trainer
 
 
@@ -30,39 +29,38 @@ def initialize_expiremts(args: Dict[str, str], experiments: Dict[str, Dict[str, 
     # initialzie the data and use deepcopy to keep original data unchanged.
     data = Dataset(args['dataset'])
     data.init_dataset(embedding_dimension)
+
+    results_exp_acc = dict()
+    results_exp_loss = dict()
     
     if args['exp'] == None:
-        results_exp_acc = dict()
-        results_exp_loss = dict()
         for exp, exp_settings in experiments.items():
             trainer = Trainer(deepcopy(data), hidden_l, epochs, embedding_dimension, lr, weight_d)
             results_acc, results_loss = trainer.exp_runner(exp_settings['sum_layers'], exp_settings['org_layers'], exp_settings['embedding_trick'], exp_settings['transfer'], exp)
             results_exp_acc.update(results_acc)
             results_exp_loss.update(results_loss)
-            timing.log('experiment done')
+            timing.log(f'{exp} done')
     
-    if args['exp'] != None:
+    else:
         exp = args['exp']
         exp_settings = experiments[exp]
         trainer = Trainer(deepcopy(data), hidden_l, epochs, embedding_dimension, lr, weight_d)
-        results_exp_acc, results_exp_loss = trainer.exp_runner(exp_settings['sum_layers'], exp_settings['org_layers'], exp_settings['embedding_trick'], exp_settings['transfer'], exp)
-        timing.log('experiment done')
+        results_acc, results_loss = trainer.exp_runner(exp_settings['sum_layers'], exp_settings['org_layers'], exp_settings['embedding_trick'], exp_settings['transfer'], exp)
+        results_exp_acc.update(results_acc)
+        results_exp_loss.update(results_loss)
+        timing.log(f'{exp} done')
 
-    # results_baseline_acc = defaultdict(list)
-    # results_baseline_loss = defaultdict(list)
+        exp = 'baseline'
+        exp_settings = experiments[exp]
+        trainer = Trainer(deepcopy(data), hidden_l, epochs, embedding_dimension, lr, weight_d)
+        results_baseline_acc, results_baseline_loss = trainer.exp_runner(exp_settings['sum_layers'], exp_settings['org_layers'], exp_settings['embedding_trick'], exp_settings['transfer'], exp)
+        results_exp_acc.update(results_baseline_acc)
+        results_exp_loss.update(results_baseline_loss)
+        timing.log(f'{exp} done')
 
-    # baseline_data = deepcopy(data)
-    # trainer = Trainer(data, hidden_l, epochs, embedding_dimension, lr, weight_d)
-    # baselineModel = BaseLayers(embedding_dimension, len(data.orgGraph.relations.keys()), hidden_l, data.num_classes)
-    # results_baseline_acc['baseline Accuracy'], results_baseline_loss['baseline Loss'] = trainer.train(baselineModel, baseline_data.orgGraph, sum_graph=False)
-    # timing.log('experiment done')
-
-    # results_acc = {**results_exp_acc, **results_baseline_acc}
-    # results_loss = {**results_exp_loss, **results_baseline_loss}
-
-    print_max_result(results_acc)
-    plot_and_save('Accuracy', args['dataset'], results_acc, epochs, args['exp'])
-    plot_and_save('Loss', args['dataset'], results_loss, epochs, args['exp'])
+    print_max_result(results_exp_acc)
+    plot_and_save('Accuracy', args['dataset'], results_exp_acc, epochs, args['exp'])
+    plot_and_save('Loss', args['dataset'], results_exp_loss, epochs, args['exp'])
 
 
 parser = argparse.ArgumentParser(description='experiment arguments')
