ee866eff28e718f82cf5d6dd0e152812047d00c7
tiddoloos <tiddoloos@hotmail.com>
2022-05-20 16:55:47 +0200

    job MUTAG update

--- jobs/job_MUTAG.sh
+++ jobs/job_MUTAG.sh
@@ -17,7 +17,7 @@ mkdir ./results
 scp -r $HOME/graphdata ./
 
 #Run Program
-python /home/loost/RGCN_MscThesis_TiddoLoos/main.py -dataset MUTAG
+python /home/loost/RGCN_MscThesis_TiddoLoos/main.py -dataset MUTAG -k 5
 
 #Copy output directory from scratch to results folder on local machine
 cd "$TMPDIR"/
