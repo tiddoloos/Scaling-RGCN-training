c57e37fc3e94c2996d593180cfc4be6d79884bc3
tiddoloos <tiddoloos@hotmail.com>
2022-05-20 16:57:25 +0200

    refactor indents in processResults.py

--- helpers/processResults.py
+++ helpers/processResults.py
@@ -8,41 +8,41 @@ from typing import Dict, List
 from torch import nn
 
 def print_max_acc(results_dict: Dict[str, List[float]]) -> None:
-        for exp, results in results_dict.items():
-                exp_strip = exp.replace(' Accuracy', '')
-                max_acc = max(results)
-                epoch = int(results.index(max_acc)) - 1 
-                max_acc = max_acc*100
-                print(f'{exp_strip.upper()}: After epoch {epoch}, Max accuracy {round(max_acc, 2)}%')
+    for exp, results in results_dict.items():
+            exp_strip = exp.replace(' Accuracy', '')
+            max_acc = max(results)
+            epoch = int(results.index(max_acc)) - 1 
+            max_acc = max_acc*100
+            print(f'{exp_strip.upper()}: After epoch {epoch}, Max accuracy {round(max_acc, 2)}%')
 
 def plot_results(metric: str, dataset: str, exp: str, epochs: int, k: int,  results_dict: Dict[str, List[int]]):
-        plt = matplotlib.pyplot
-        epoch_list = [i for i in range(epochs)]
-        for key, result in results_dict.items():
-                y = result
-                x = epoch_list 
-                plt.plot(x, y, label = key)
+    plt = matplotlib.pyplot
+    epoch_list = [i for i in range(epochs)]
+    for key, result in results_dict.items():
+            y = result
+            x = epoch_list 
+            plt.plot(x, y, label = key)
 
-        plt.title(f'{metric} on {dataset} dataset during training epochs')
-        plt.xlabel('Epochs')
-        plt.ylabel('Accuracy/Loss')
-        plt.grid(color='b', linestyle='-', linewidth=0.1)
-        plt.margins(x=0)
-        plt.legend(loc='best')
-        plt.xticks(np.arange(0, len(epoch_list), 5))
-        plt.xlim(xmin=0)
-        plt.yticks(np.arange(0, 1.1, 0.1))
-        plt.ylim(ymin=0)
-        dt = datetime.now()
-        str_date = dt.strftime('%d%B%Y-%H%M%S')
-        plt.savefig(f'./results/{dataset}_{metric}_k={k}_{exp}_{str_date}.png', format='png')
-        plt.show()
+    plt.title(f'{metric} on {dataset} dataset during training epochs')
+    plt.xlabel('Epochs')
+    plt.ylabel('Accuracy/Loss')
+    plt.grid(color='b', linestyle='-', linewidth=0.1)
+    plt.margins(x=0)
+    plt.legend(loc='best')
+    plt.xticks(np.arange(0, len(epoch_list), 5))
+    plt.xlim(xmin=0)
+    plt.yticks(np.arange(0, 1.1, 0.1))
+    plt.ylim(ymin=0)
+    dt = datetime.now()
+    str_date = dt.strftime('%d%B%Y-%H%M%S')
+    plt.savefig(f'./results/{dataset}_{metric}_k={k}_{exp}_{str_date}.png', format='png')
+    plt.show()
 
 def save_to_json(metric: str, dataset: str, exp: str, k: int, results_dict: Dict[str, List[int]]) -> None:
-        dt = datetime.now()
-        str_date = dt.strftime('%d%B%Y-%H%M%S')
-        with open(f'./results/{dataset}_{metric}_k={k}_{exp}_{str_date}.json', 'w') as write_file:
-                json.dump(results_dict, write_file, indent=4)
+    dt = datetime.now()
+    str_date = dt.strftime('%d%B%Y-%H%M%S')
+    with open(f'./results/{dataset}_{metric}_k={k}_{exp}_{str_date}.json', 'w') as write_file:
+            json.dump(results_dict, write_file, indent=4)
 
 def print_trainable_parameters(model: nn.Module, exp: str) -> int:
     trainable_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
