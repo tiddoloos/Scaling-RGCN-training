86ee54c6357f01da3e808deaf4a3cf12b671f496
tiddoloos <tiddoloos@hotmail.com>
2022-05-31 11:31:21 +0200

    job am + data name changes

--- jobs/job_AM.sh
+++ jobs/job_AM.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#SBATCH --partition=gpu_titanrtx
-#SBATCH --gres=gpu:1
+#SBATCH --partition=gpu_titanrtx_shared
+#blub --gres=gpu:1
 #SBATCH --job-name=AM_run
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
