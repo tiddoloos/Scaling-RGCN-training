50aa83c02ec37481d3e9674a96690cba2d7b8c1e
tiddoloos <tiddoloos@hotmail.com>
2022-06-02 11:49:13 +0200

    kbisim job

--- jobs/job_kbsim.sh
+++ jobs/job_kbsim.sh
@@ -13,6 +13,7 @@ module load 2021
 
 cd $TMPDIR
 cp ~/scala_setup/fluid-spark .
+cd ./scala_setup/fluid-spark
 
 singularity run --pwd $PWD --writable-tmpfs --bind ./orientdb/databases:/orientdb/databases,./orientdb/backup:/orientdb/backup,./orientdb/config/orientdb-server-config.xml:/orientdb/config/orientdb-server-config.xml  ./fluid-spark/myorientdb.sif
 
