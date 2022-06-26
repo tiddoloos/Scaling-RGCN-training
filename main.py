d592efcd146fe961890153878aa001b922439f6a
tiddoloos <tiddoloos@hotmail.com>
2022-06-26 17:37:03 +0200

    refacor modelTrainer.py and main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ from model.modelTrainer import Trainer
 from distutils.util import strtobool
 
 
-def initialize_expirements(configs: Dict[str, Union[bool, str, int, float]], methods: Dict[str, Dict[str, Callable]], org_path: str, sum_path: str, map_path: str) -> None:
+def initialize_expirements(configs: Dict[str, Union[bool, str, int, float]], experiments: Dict[str, Dict[str, Callable]], org_path: str, sum_path: str, map_path: str) -> None:
     """This functions executes experiments to scale graph training with RGCN. 
     After training on summary graphs, the weights and node embeddings of 
     the summary model will be transferd to a new model for training on the 
@@ -26,22 +26,28 @@ def initialize_expirements(configs: Dict[str, Union[bool, str, int, float]], met
 
     results = Results()
 
+    experiment_names = list(experiments.keys())
+    if configs['exp'] != None:
+        experiment_names = [configs['exp']]
+
+    # create summaries if needed
+    if configs['create_attr_sum']:
+        timing.log('Creating graph summaries...')
+        create_sum_map(org_path, sum_path, map_path, dataset)
+
     for j in range(configs['i']):
-
-        # create summaries
-        if configs['create_attr_sum']:
-            timing.log('Creating graph summaries...')
-            create_sum_map(org_path, sum_path, map_path, dataset)
-
+        
         # initialzie the data and use deepcopy when using data to keep original data unchanged.
         timing.log('...Making Graph data...')
         data = Dataset(org_path, sum_path, map_path)
         data.init_dataset()
-        
-        if configs['exp'] == None:
-            trainer = Trainer(deepcopy(data), configs['hl'], configs['epochs'], configs['emb'], configs['lr'], weight_d=0.00005)
-            trainer.train_summaries()
-            for exp, exp_settings in methods['experiments'].items():
+    
+        # run experiment(s)
+        trainer = Trainer(deepcopy(data), configs['hl'], configs['epochs'], configs['emb'], configs['lr'], weight_d=0.00005)
+        trainer.train_summaries()
+        for exp in experiment_names:
+            # if exp != 'baseline':
+                exp_settings = experiments[exp]
                 results.add_key(exp)
                 timing.log(f'Start {exp} Experiment')
                 results_acc, results_loss, results_f1_w, results_f1_m, test_acc, test_micro, test_macro = trainer.train_original(exp_settings['org_layers'], exp_settings['embedding_trick'], configs, exp)
@@ -54,43 +60,6 @@ def initialize_expirements(configs: Dict[str, Union[bool, str, int, float]], met
                 results.test_f1_macro[f'Test F1 macro {exp}'].append(test_macro) 
 
                 timing.log(f'{exp} experiment done')
-        
-        elif configs['exp'] != 'baseline':
-                exp = configs['exp']
-                results.add_key(exp)
-                exp_settings = methods['experiments'][exp]
-                trainer = Trainer(deepcopy(data), configs['hl'], configs['epochs'], configs['emb'], configs['lr'], weight_d=0.00005)
-
-                timing.log('Training on summary Graphs')
-                trainer.train_summaries()
-
-                timing.log(f'Start {exp} Experiment')
-                results_acc, results_loss, results_f1_w, results_f1_m, test_acc, test_micro, test_macro = trainer.train_original(exp_settings['org_layers'], exp_settings['embedding_trick'], configs, exp)
-                timing.log(f'{exp} experiment done')
-
-                for result in [results_acc, results_loss, results_f1_w, results_f1_m]:
-                    results.update_run_results(result, exp)
-
-                results.test_accs[f'Test acc {exp}'].append(test_acc)
-                results.test_f1_weighted[f'Test F1 weighted {exp}'].append(test_micro)
-                results.test_f1_macro[f'Test F1 macro {exp}'].append(test_macro) 
-
-
-        exp = 'baseline'
-        exp_settings = methods[exp]
-        results.add_key(exp)
-        trainer = Trainer(deepcopy(data), configs['hl'], configs['epochs'], configs['emb'], configs['lr'], weight_d=0.00005)
-
-        timing.log(f'Start {exp} Experiment')
-        results_acc, results_loss, results_f1_w, results_f1_m, test_acc, test_micro, test_macro = trainer.train_original(exp_settings['org_layers'], exp_settings['embedding_trick'], configs, exp)
-        timing.log(f'{exp} experiment done')
-
-        for result in [results_acc, results_loss, results_f1_w, results_f1_m]:
-            results.update_run_results(result, exp)
-
-        results.test_accs[f'Test acc {exp}'].append(test_acc)
-        results.test_f1_weighted[f'Test F1 weighted {exp}'].append(test_micro)
-        results.test_f1_macro[f'Test F1 macro {exp}'].append(test_macro) 
 
     results.process_results(configs)
 
@@ -111,12 +80,10 @@ if __name__=='__main__':
     
     configs = vars(parser.parse_args())
 
-    methods = {'baseline': {
-                    'org_layers': Emb_Layers, 'embedding_trick': None},
-                'experiments': {
+    experiments = {'baseline':{'org_layers': Emb_Layers, 'embedding_trick': None},
                     'summation': {'org_layers': Emb_Layers, 'embedding_trick': sum_embeddings},
                     'mlp': {'org_layers': Emb_MLP_Layers, 'embedding_trick': concat_embeddings},
-                    'attention': {'org_layers': Emb_ATT_Layers, 'embedding_trick': stack_embeddings}}}
+                    'attention': {'org_layers': Emb_ATT_Layers, 'embedding_trick': stack_embeddings}}
 
     dataset = configs['dataset']
     sum = configs['sum']
@@ -124,4 +91,4 @@ if __name__=='__main__':
     sum_path = f'graphdata/{dataset}/{sum}/sum/'
     map_path = f'graphdata/{dataset}/{sum}/map/'
 
-    initialize_expirements(configs, methods, path, sum_path, map_path)
+    initialize_expirements(configs, experiments, path, sum_path, map_path)
--- model/modelTrainer.py
+++ model/modelTrainer.py
@@ -104,13 +104,12 @@ class Trainer:
 
         orgModel = org_layers(len(self.data.orgGraph.relations.keys()), self.hidden_l, self.data.num_classes, self.data.orgGraph.num_nodes, self.emb_dim, configs['num_sums'])
         
-        print(configs['e_trans'])
         if exp != 'baseline' and configs['e_trans'] == True:
             embedding_trick(self.data.orgGraph, self.data.sumGraphs, self.emb_dim)
             orgModel.load_embedding(self.data.orgGraph.training_data.embedding.clone())
             print('Loaded pre trained embedding')
 
-        if exp != 'baseline' and configs['w_trans']  == True:
+        if exp != 'baseline' and configs['w_trans'] == True:
             self.transfer_weights(orgModel)
     
         print('Training on Orginal Graph...')
