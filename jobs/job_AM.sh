56722991fdecab1b979351e2db8d68df4b9c5869
tiddoloos <tiddoloos@hotmail.com>
2022-07-06 00:11:52 +0200

    job AM

--- jobs/job_AM.sh
+++ jobs/job_AM.sh
@@ -17,7 +17,7 @@ mkdir ./results
 scp -r $HOME/graphdata ./
 
 #Run Program
-python /home/loost/RGCN_MscThesis_TiddoLoos/main.py -dataset AM -i 1 
+python /home/loost/RGCN_MscThesis_TiddoLoos/main.py -dataset AM -i5 
 
 #Copy output directory from scratch to results folder on local machine
 cd "$TMPDIR"/
