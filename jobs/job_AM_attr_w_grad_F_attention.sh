50a834d6c8fcdf0dfd37099bfc252c6cfcb76be5
tiddoloos <tiddoloos@hotmail.com>
2022-07-13 11:39:42 +0200

    job names

--- jobs/job_AM_attr_w_grad_F_attention.sh
+++ jobs/job_AM_attr_w_grad_F_attention.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --partition=normal
 #SBATCH --constraint=silver_4110
-#SBATCH --job-name=AM_CPU_run
+#SBATCH --job-name=job_AM_attr_w_grad_F_attention
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=2
