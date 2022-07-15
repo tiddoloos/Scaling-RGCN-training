366254b601a1782a1d40dbf063fdc4dc2ef6080f
tiddoloos <tiddoloos@hotmail.com>
2022-07-15 10:16:44 +0200

    job AM e feeze false

--- jobs/job_AM_bisim_w_grad_F_summation.sh
+++ jobs/job_AM_bisim_w_grad_F_summation.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --partition=normal
 #SBATCH --constraint=silver_4110
-#SBATCH --job-name=AM_bisim_w_grad_F_attention
+#SBATCH --job-name=AM_bisim_w_grad_F_summation
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=2
@@ -18,7 +18,7 @@ scp -r $HOME/graphdata ./
 scp -r $HOME/RGCN_MscThesis_TiddoLoos/baselines ./
 
 #Run Program
-python /home/loost/RGCN_MscThesis_TiddoLoos/main.py -dataset AM -i 5 -w_grad False -exp attention -sum bisim
+python /home/loost/RGCN_MscThesis_TiddoLoos/main.py -dataset AM -i5 -w_grad False -exp summation -e_freeze False -sum bisim
 
 #Copy output directory from scratch to results folder on local machine
 cd "$TMPDIR"/
