be901b58b353f91457e52df148ed9eafdcbbd3a5
tiddoloos <tiddoloos@hotmail.com>
2022-08-01 17:02:00 +0200

    refactor

--- model/evaluation.py
+++ model/evaluation.py
@@ -8,10 +8,10 @@ from sklearn.metrics import classification_report, f1_score, accuracy_score
 def calc_acc(pred: Tensor, x: Tensor, y: Tensor) -> float:
     return accuracy_score(y, pred[x])
 
-def calc_f1(pred: Tensor, x: Tensor, y: Tensor, avg='weighted') -> float:
+def calc_f1(pred: Tensor, x: Tensor, y: Tensor, avg: str='weighted') -> float:
     return f1_score(y, pred[x], average=avg, zero_division=0)
 
-def evaluate(model: nn.Module, activation, traininig_data: Data, x: Tensor, y: Tensor, report=False) -> float:
+def evaluate(model: nn.Module, activation, traininig_data: Data, x: Tensor, y: Tensor, report=False) -> Tuple[float]:
     pred = model(traininig_data, activation)
     if activation != torch.sigmoid:
         softmax = nn.Softmax(dim=1)
@@ -36,7 +36,7 @@ def ce_loss(pred: Tensor, targets: Tensor) -> Tensor:
     output = loss_f(pred, targets)
     return output
 
-def bce_loss(pred, targets):
+def bce_loss(pred: Tensor, targets: Tensor) -> Tensor:
     loss_f = nn.BCELoss()
     output = loss_f(pred, targets)
     return output
